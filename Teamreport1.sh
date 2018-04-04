730925a392b17d998ed3545f29d0e5dde8128308
Joval14 <jotaval@ooutlook.com>
2018-04-04 03:33:03 +0000

    Teamreport1.sh

---
+++ Teamreport1.sh
@@ -0,0 +1,18 @@
+# Week-12
+#Team Report 1
+
+#Creating a Web Server (APache)
+
+#sudo apt-get update
+
+#ifconfig (to check IP)
+#cd /var/www/html (to host our web)
+#ls (show the content of the web)
+#sudo nano <name of the web>.html
+
+#If you type local host you shoul be able to see your web
+
+#sudo nano /etc/host (change the host name)
+
+
+
